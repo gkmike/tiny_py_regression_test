146a9c44c65a3e9bb4882238bb1badcae17800ea
gkmike68 <gkmike68@hotmail.com>
2019-11-10 18:02:49 +0800

    change usage

--- example.py
+++ example.py
@@ -1,19 +1,17 @@
 from tiny_py_rt import regression_test
 
 rt = regression_test()
-build_job = rt.create_job("build_code")
+test = rt.create_test("code_test")
+build_job = test.create_job("build_code")
 build_job.files.links.append("example/src/*")
 build_job.files.copys.append("example/pat/*")
 build_job.env_vars.append(["OPT", "-O2"])
 build_job.cmds.steps.append("make")
 build_job.cmds.result_checks.append("test a.out")
-rt.add_test(build_job)
 
-sim_job = rt.create_job("run program")
-sim_job.add_dependency(build_job)
+sim_job = test.create_job("run_program")
 sim_job.files.links.append(build_job.working_dir())
 sim_job.cmds.steps.append("./a.out > result.txt")
-sim_job.cmds.result_checks.append("grep 'hello world' result.txt")
-rt.add_test(sim_job)
+sim_job.cmds.result_checks.append("diff golden.txt result.txt")
 
 rt.process()
--- tiny_py_rt.py
+++ tiny_py_rt.py
@@ -1,17 +1,39 @@
+import sys
+import argparse
+
 class regression_test:
     def __init__(self):
-        self._job = []
+        self._test = []
+    def create_test(self, test_name):
+        t = test(test_name)
+        self.add_test(t)
+        return test(test_name)
+    def add_test(self, t):
+        self._test.append(t)
+    def show_test(self):
+        print("[test_name]")
+        for i in self._test:
+            print(i.test_name)
+    def process(self):
+        parser = argparse.ArgumentParser()
+        #parser.add_argument('')
+        parser.parse_args()
+        if len(sys.argv) == 1:
+            self.show_test()
+        #self._cwd_proc()
+        #self._gen_pytest()
+        #self._run_pytest()
+
+class test:
+    def __init__(self, test_name):
+        self.test_name = test_name
+        pass
     def create_job(self, job_name):
         return job(job_name)
-    def add_test(self, job):
-        self._job.append(job)
-    def process(self):
-        pass
-
 
 class job:
     def __init__(self, job_name):
-        self._job_name = job_name
+        self.job_name = job_name
         self._dep_jobs = []
         self.files = lambda: None
         self.files.links = []
