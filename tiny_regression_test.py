9a22fc587d63c05e8e6628b7d4d275623e25c8c0
gkmike68 <gkmike68@hotmail.com>
2019-11-30 23:22:49 +0800

    shorten command, add copy_from

--- tiny_regression_test.py
+++ tiny_regression_test.py
@@ -2,19 +2,21 @@ import sys
 import os
 import argparse
 import subprocess as sub
+import copy
 
 class test_base:
     def __init__(self, name):
         self._name = name
-        self._cwd = None
         self._skip = False
-    def set_cwd(self, cwd):
-        self._cwd = cwd
+        self._parent = None
     def get_cwd(self):
-        return self._cwd
+        if self._parent:
+            return self._parent.get_cwd() + "/" + self.get_name()
+        else:
+            return self.get_name()
     def get_name(self):
         return self._name
-    def status(self):
+    def get_status(self):
         if self._skip:
             return " (skipped)"
         return ""
@@ -23,18 +25,17 @@ class regression_test(test_base):
     def __init__(self, top_name):
         super().__init__(top_name)
         self._tests = []
-        self.set_cwd(top_name)
     def create_test(self, test_name):
         t = test(test_name)
-        t.set_cwd(self.get_cwd() + '/' + test_name)
+        t._parent = self
         self._tests.append(t)
         return t
     def show_test(self):
-        print("[test list]")
+        print(self.get_name() + " [test list]")
         for t in self._tests:
-            print (t.get_name() + t.status())
+            print (t.get_name() + t.get_status() + " [test] [path=" + t.get_cwd() + "]")
             for j in t._jobs:
-                print("  " + j.get_name() + j.status())
+                print("    " + j.get_name() + j.get_status() + " [job] [path=" + t.get_cwd() + "]")
     def process(self):
         parser = argparse.ArgumentParser()
         parser.add_argument('-a', '--all', action='store_true', help='run all test')
@@ -56,7 +57,7 @@ class regression_test(test_base):
         self.show_test()
         for t in self._tests:
             if not t._skip: 
-                t.run()
+                t._run()
         #self._cwd_proc()
         #self._gen_pytest()
         #self._run_pytest()
@@ -67,58 +68,69 @@ class test(test_base):
         self._jobs = []
     def create_job(self, job_name):
         j = job(job_name)
-        j.set_cwd(self.get_cwd() + '/' + job_name)
+        j._parent = self
         self._jobs.append(j)
         return j
-    def run(self):
+    def _run(self):
         print("running test " + self.get_name())
         for j in self._jobs:
             if not j._skip: 
-                j.run() 
+                j._run() 
+        
 
 class job(test_base):
     def __init__(self, job_name):
         super().__init__(job_name)
         self.file = file()
-        self.env = env_vars()
+        self.env = env()
         self.cmd = cmd()
-    def run(self):
+    def _run(self):
         print("running job " + self.get_name())
         cwd = self.get_cwd()
         sub.run('rm -rf ' + cwd, shell=True, check=True)
         sub.run('mkdir -p ' + cwd, shell=True, check=True)
-        self.file.prepare(cwd)
-        self.env.setup()
-        self.cmd.execute(cwd)
-
+        self.file._put(cwd)
+        self.env._setup()
+        self.cmd._run(cwd)
+    def copy_from(self, j):
+        self.file = copy.deepcopy(j.file)
+        self.env = copy.deepcopy(j.env)
+        self.cmd = copy.deepcopy(j.cmd)
 
 class file:
     def __init__(self):
-        self.links = []
-        self.copys = []
-    def prepare(self, cwd):
-        for f in self.links:
-            sub.run("ln -sf " + os.path.abspath(f) + " .", shell=True, check=True, cwd=cwd)
-        for f in self.copys:
-            sub.run("cp -rf " + os.path.abspath(f) + " .", shell=True, check=True, cwd=cwd)
+        self.links = file_handles()
+        self.copys = file_handles()
+    def _put(self, cwd):
+        self.links._put("ln -sf", cwd)
+        self.copys._put("cp -rf", cwd)
 
-
-class env_vars:
+class file_handles:
+    def __init__(self):
+        self._list = []
+    def add(self, path):
+        self._list.append(path)
+        return self
+    def _put(self, sh_cmd, cwd):
+        for f in self._list:
+            sub.run(sh_cmd + " " + os.path.abspath(f) + " .", shell=True, check=True, cwd=cwd)
+            
+class env:
     def __init__(self):
         self._env = {}
-    def append(self, env):
-        self._env.update(env)
-    def setup(self):
+    def set(self, var, val):
+        self._env[var] = str(val)
+        return self
+    def _setup(self):
         for k,v in self._env.items():
             os.environ[k] = v
         
 class cmd():
     def __init__(self):
-        self.steps = []
-        self.result_checks = []
-    def execute(self, cwd):
-        for s in self.steps:
+        self._steps = []
+    def add(self, cmd):
+        self._steps.append(cmd)
+        return self
+    def _run(self, cwd):
+        for s in self._steps:
             sub.run(s, shell=True, check=True, cwd=cwd)
-        for s in self.result_checks:
-            sub.run(s, shell=True, check=True, cwd=cwd)
-            
